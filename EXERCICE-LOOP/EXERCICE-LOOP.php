433f7bb23cc9a0908c4239e39e9d0c0828082d5a
Holma23 <44214084+Holma23@users.noreply.github.com>
2018-11-05 18:44:53 +0100

    Add files via upload

---
+++ EXERCICE-LOOP/EXERCICE-LOOP.php
@@ -0,0 +1,29 @@
+<?php
+$nbre=23;
+for($i=1;$i<=$nbre;$i++)
+{
+    echo $i."<br>";
+}
+
+?>
+<br>
+<?php
+$i=1;
+$nbre=5;
+while($i<=$nbre)
+{
+    echo $i."<br>";
+    $i++;
+}
+?>
+<br>
+<?php
+$i=1;
+$nbre=8;
+do{
+    echo $i."<br>";
+    $i++;
+}
+while($i<=$nbre);
+?>
+
